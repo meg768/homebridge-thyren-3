fce9523f1f1e3396c9c24bfa6ddc7fe0f1728e69
meg768 <meg768@users.noreply.github.com>
2017-12-14 15:48:25 +0100

    -

--- src/thermometer-hygrometer.js
+++ src/thermometer-hygrometer.js
@@ -5,5 +5,46 @@ var TelldusThermometer = require('./thermometer.js');
 module.exports = class TelldusThermometerHygrometer extends TelldusThermometer {
 
 
+    constructor(platform, config, device) {
+        super(platform, config, device);
 
+        this.setupHumiditySensor();
+        this.setupTemperatureSensor();
+    }
+
+    setupTemperatureSensor() {
+        this.services.temperatureSensor = new this.Service.TemperatureSensor(this.name, this.device.name);
+        var characteristics = this.services.temperatureSensor.getCharacteristic(this.Characteristic.CurrentTemperature);
+
+        characteristics.setProps({minValue: -50});
+
+        characteristics.on('get', (callback) => {
+            callback(null, this.getTemperature());
+        });
+
+        services.push(service);
+
+        return services;
+    }
+
+    setupHumiditySensor() {
+        this.service.humiditySensor = new this.Service.HumiditySensor(this.name, this.device.name);
+
+        var characteristics = this.service.humiditySensor.getCharacteristic(this.Characteristic.CurrentRelativeHumidity);
+
+        characteristics.updateValue(this.getHumidity());
+
+        characteristics.on('get', (callback) => {
+            callback(null, this.getHumidity());
+        });
+    }
+
+    getTemperature() {
+        return this.device.temperature ? parseFloat(this.device.temperature) : undefined;
+
+    }
+
+    getHumidity() {
+        return this.device.humidity;
+    }
 };
